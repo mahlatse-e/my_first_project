9f66e4b7f430f6ecce83906b682443c29e2f0e3f
mahlatse <73551883+mahlatse-e@users.noreply.github.com>
2020-11-06 06:45:53 +0200

    panel

--- admin/includes/panel.php
+++ admin/includes/panel.php
@@ -3,10 +3,10 @@
 		<div class="row">
 			<div class="col-md-12" >
 				
-				<h1> <i class="fa fa-user"></i>Welcome, <?php echo $username;?> you are an <?php echo $role; ?></a>
+				<h1> <i class="glyphicon glyphicon-user" aria-hidden="true"></i>Welcome, <?php echo $username;?> you are an <?php echo $role; ?></a>
                    
 				   
-				<h1>press <a href="index.php?logout"><i class="fa fa-fw fa-power-off"></i></a> to exit</h1>
+				<h1>press <a href="index.php?logout"><i><img src="bootstrap/power2.jpg" alt="" width="50px"></i></a> to exit</h1>
 				 
 			</div>
 		</div>
